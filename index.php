9483447ff9b28d07a44367dcee01a6e49ac7bdc9
suyu06 <suyu.zhang@johnabbottcollege.net>
2025-03-17 21:17:03 -0400

    php fuction

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-this is our amazing custom theme.
+This is our amazing custom theme.
+<?php 
+    function greet($name,$color){
+        echo "<p>Hi,my name is $name and my favorite color is $color</p>";
+    }
+    greet("Jean", "vert");
+    greet("martin", "bleu");
+?>
