7201e073bda96283b87ba767f29cb6e236abf4fb
suyu06 <suyu.zhang@johnabbottcollege.net>
2025-03-18 23:15:56 -0400

    famous loop1

--- index.php
+++ index.php
@@ -1,28 +1,36 @@
-This is our amazing custom theme.
+<!-- This is our amazing custom theme. -->
 <!-- function & arrays-->
-<?php 
+<!-- <?php 
     function greet($name,$color){
         echo "<p>Hi,my name is $name and my favorite color is $color</p>";
     }
     greet("Jean", "vert");
-    greet("martin", "bleu");    
- 
+    greet("martin", "bleu"); 
 ?>
 <h1><?php bloginfo("name"); ?></h1>
 <h1><?php bloginfo("description"); ?></h1>
 
 <?php 
     $count=1;
-    while($count<101){
+    while($count<10){
         echo "<li>$count</li>";
         $count++;}
 ?>
 <?php     
     $names= array("Matin","Pierre","Ziba","suzy");
-    $count=1;
-    while($count<101){
-        echo "<li>$count</li>";
-        $count++;}
+    $n=0;
+    while($n<count($names)){
+        echo "<li>$names[$n]</li>";
+        $n++;}
 
+?> -->
+
+
+<?php 
+
+        while(have_posts()){
+            the_post();?>
+        <h2>hello</h2>
+<?php         }
 ?>
 
