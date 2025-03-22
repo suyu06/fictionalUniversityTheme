0ee05e3d659c77700bc51aa01bbfe5b0b151640b
suyu06 <suyu.zhang@johnabbottcollege.net>
2025-03-22 16:01:21 -0400

    create functions.php and use function to call the style.css

--- header.php
+++ header.php
@@ -1,1 +1,13 @@
-<h1>Greetings from header.php</h1>
+<!-- <h1>Greetings from header.php</h1> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <h1>Fictional University</h1>
+</body>
+</html>
