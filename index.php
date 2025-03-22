16840cd481ebef27202f3660b017ecd78c842f3a
suyu06 <suyu.zhang@johnabbottcollege.net>
2025-03-21 23:30:20 -0400

    add link to each post

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 <?php 
         while(have_posts()){
             the_post();?>
-      <h2><?php  the_title();?></h2>
+      <h2><a href= "<?php the_permalink() ?>"><?php  the_title();?></a></h2>
       <p><?php the_content() ;?></p> 
 <?php         }
 ?>
