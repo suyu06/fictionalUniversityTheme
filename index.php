0847eed8107915c4636939d043a9787e4f3100fd
suyu06 <suyu.zhang@johnabbottcollege.net>
2025-03-21 23:26:56 -0400

    the famous loop with 3 posts and contents

--- index.php
+++ index.php
@@ -27,10 +27,10 @@
 
 
 <?php 
-
         while(have_posts()){
             the_post();?>
-        <h2>hello</h2>
+      <h2><?php  the_title();?></h2>
+      <p><?php the_content() ;?></p> 
 <?php         }
 ?>
 
