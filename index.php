1a02046999f65dec417f9af1fad11c42e798000d
suyu06 <suyu.zhang@johnabbottcollege.net>
2025-03-22 15:18:45 -0400

    create new files header and footer then write them into index, page and single file

--- index.php
+++ index.php
@@ -24,13 +24,15 @@
         $n++;}
 
 ?> -->
+<?php  get_header();
 
-
-<?php 
         while(have_posts()){
             the_post();?>
       <h2><a href= "<?php the_permalink(); ?>"><?php  the_title();?></a></h2>
       <p><?php the_content() ;?></p> 
+      
 <?php         }
+
+    get_footer();
 ?>
 
