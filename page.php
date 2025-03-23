454f90b80425d02c8e09d08c65d4ef9427ac19c3
suyu06 <suyu.zhang@johnabbottcollege.net>
2025-03-23 11:54:52 -0400

    realise the redirection between  index page and about/privacy page

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php 
       get_header();
-        while(have_posts()){
+      while(have_posts()){
             the_post();?>
       <!-- <h1>This is a page, not a post</h1>
       <h2><?php  the_title();?></h2>
@@ -13,7 +13,7 @@
         <!-- <h1 class="page-banner__title">Our History</h1> -->
         <h1 class="page-banner__title"><?php the_title();?></h1>
         <div class="page-banner__intro">
-          <p>Learn how the school of your dreams got started.</p>
+          <p>Don't forget to replace me later</p>
         </div>
       </div>
     </div>
@@ -21,17 +21,17 @@
     <div class="container container--narrow page-section">
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link" href="<?php echo site_url('/about-us') ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
         </p>
       </div>
-
-      <div class="page-links">
+<!-- leave it in place on 0501-->
+      <!-- <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
           <li class="current_page_item"><a href="#">Our History</a></li>
           <li><a href="#">Our Goals</a></li>
         </ul>
-      </div>
+      </div> -->
 
       <div class="generic-content">
          <!-- replace the static content with dynamic content according to different pages -->
