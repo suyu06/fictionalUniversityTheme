0b39bd94e3e01a799234ad0a4ffe0d7ee8014d33
suyu06 <suyu.zhang@johnabbottcollege.net>
2025-03-24 20:26:40 -0400

    add if function to decide whether  display the bakc to tab, and use php function to replace statci code

--- page.php
+++ page.php
@@ -19,12 +19,27 @@
     </div>
 
     <div class="container container--narrow page-section">
-      <div class="metabox metabox--position-up metabox--with-home-link">
+      <!-- only show "return to parent page,if it's in the child page" -->
+      <?php
+      $theParent = wp_get_post_parent_id(get_the_ID());
+       if($theParent){?>
+        <div class="metabox metabox--position-up metabox--with-home-link">
+        <!-- <p>
+          <a class="metabox__blog-home-link"
+           href="<?php echo site_url('/about-us') ?>">
+           <i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a>
+           <span class="metabox__main">Our History</span>
+        </p> -->
         <p>
-          <a class="metabox__blog-home-link" href="<?php echo site_url('/about-us') ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+          <a class="metabox__blog-home-link"
+           href="<?php echo get_permalink($theParent); ?>">
+           <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent);?></a>
+           <span class="metabox__main"><?php the_title();?></span>
         </p>
       </div>
-<!-- leave it in place on 0501-->
+       
+     
+      <?php } ?>
       <!-- <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
