da25f34991ade51609fdb10287fce31bb6c2ed98
suyu06 <suyu.zhang@johnabbottcollege.net>
2025-04-13 16:45:44 -0400

    copy the banner part from page.php and change the title and intro part into static code

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 get_header();
 ?>
-
+<div class="page-banner">
+      <div class="page-banner__bg-image"
+       style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg')?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <!-- replace the static title with dynamic title according to different pages -->
+        <!-- <h1 class="page-banner__title">Our History</h1> -->
+        <h1 class="page-banner__title">Welcome to our blog!</h1>
+        <div class="page-banner__intro">
+          <p>Keep up with our latest news.</p>
+        </div>
+      </div>
+    </div>
 <?php
 get_footer();
 ?>
