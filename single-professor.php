d83a2933d12e3a162e95b71250d4b47073c50a98
suyu06 <suyu.zhang@johnabbottcollege.net>
2025-04-27 16:50:51 -0400

    add subtitle and customizes imager for the banner

--- single-professor.php
+++ single-professor.php
@@ -4,44 +4,50 @@ while (have_posts()) {
       the_post(); ?>
       <div class="page-banner">
             <div class="page-banner__bg-image"
-                  style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg') ?>)"></div>
+                  style="background-image: 
+                  url(<?php
+                        $pageBannerImage = get_field('page_banner_background_image');
+                        echo $pageBannerImage['sizes']['pageBanner'] ?>)">
+            </div>
+            <!-- <?php print_r($pageBannerImage) ?> -->
             <div class="page-banner__content container container--narrow">
                   <!-- replace the static title with dynamic title according to different pages -->
                   <!-- <h1 class="page-banner__title">Our History</h1> -->
                   <h1 class="page-banner__title"><?php the_title(); ?></h1>
+
                   <div class="page-banner__intro">
-                        <p>Keep up with our latest news.</p>
+                        <p><?php the_field('pager_banner_subtitle1') ?></p>
                   </div>
             </div>
       </div>
-      <div class="container container--narrow page-section">            
+      <div class="container container--narrow page-section">
             <div class="generic-content">
                   <div class="row group">
                         <div class="one-third">
-                              <?php the_post_thumbnail('professorPortrait'); ?>                           
+                              <?php the_post_thumbnail('professorPortrait'); ?>
                         </div>
                         <div class="two-thirds">
                               <?php the_content(); ?>
+                        </div>
                   </div>
+                  <?php
+                  $relatedPrograms = get_field('related_programs');
+                  // print_r($relatedPrograms);
+                  if ($relatedPrograms) {
+                        echo '<hr class="section-break">';
+                        echo '<h2 class="headline headline--medium">Programs Involved</h2>';
+                        echo '<ul class="link-list min-list">';
+                        foreach ($relatedPrograms as $program) { ?>
+                              <li><a href="<?php echo get_the_permalink($program); ?>">
+                                          <?php echo get_the_title($program); ?>
+                                    </a></li>
+                  <?php }
+                        echo '</ul>';
+                  }
+
+                  ?>
             </div>
-            <?php
-            $relatedPrograms = get_field('related_programs');
-            // print_r($relatedPrograms);
-            if ($relatedPrograms) {
-                  echo '<hr class="section-break">';
-                  echo '<h2 class="headline headline--medium">Programs Involved</h2>';
-                  echo '<ul class="link-list min-list">';
-                  foreach ($relatedPrograms as $program) { ?>
-                        <li><a href="<?php echo get_the_permalink($program); ?>">
-                                    <?php echo get_the_title($program); ?>
-                              </a></li>
-            <?php }
-                  echo '</ul>';
-            }
 
-            ?>
-      </div>
-
-<?php         }
+      <?php         }
 get_footer();
-?>
+      ?>
